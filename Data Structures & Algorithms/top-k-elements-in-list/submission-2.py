4eb02ce79c84142e052e62ce126143b91daf3733
abishek1310 <159773374+abishek1310@users.noreply.github.com>
2026-06-17 19:53:51 -0400

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dict={}
+        for i in nums:
+            dict[i] = 1+ dict.get(i,0)
+        l1=[]
+        for i,j in dict.items():
+            l1.append([j,i])
+        l1.sort()
+        l2=[]
+        while len(l2) <k:
+            l2.append(l1.pop()[1])
+        return l2
+        
